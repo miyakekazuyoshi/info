3fee051f0b00885cb85b027233b1df77943535d4
miyakekazuyoski <m.6945.k.38k@gmail.com>
2016-05-09 15:00:54 +0900

    ログイン処理修正

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -43,7 +43,7 @@ class AppController extends Controller {
 				'action'=>'index'
 			),
 			'logoutRedirect'=>array(
-				'controller'=>'Users',
+				'controller'=>'users',
 				'action'=>'login',
 			),
 			'authenticate' => array(
